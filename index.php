8301ff7fd0f395d6d04a5f271af94f71f69f4adc
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2019-02-26 15:44:35 -0600

    Added color to nav bar and main image

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php get_header(); ?>
+<!--The main image for the website-->
+<section id="main_section">
+    <div id="main_image">
+        <img id="mainImage"src="<?php echo get_template_directory_uri() . '/dest/img/code-1076533_1280.jpg' ?>" alt="Web Development Environment">
+    </div>
+    <div id="main_title">
+
+    </div>
+</section>
 
 <?php get_footer(); ?>
