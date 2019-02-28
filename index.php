9818db93addc692edf4ec868d0b71f4100a79f1a
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2019-02-27 21:46:24 -0600

    Added main_section video and deleted unnecessary css file

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php get_header(); ?>
 <!--The main image for the website-->
 <section id="main_section">
-    <div id="main_image">
-        <img id="mainImage"src="<?php echo get_template_directory_uri() . '/dest/img/code-1076533_1280.jpg' ?>" alt="Web Development Environment">
-    </div>
-    <div id="main_title">
-
-    </div>
+    <video width="100%" height="100%" playsinline autoplay muted loop>
+        <source src="<?php echo get_template_directory_uri() . '/dest/mov/Atoms.mp4' ?>" type="video/mp4">
+        Sorry, your browser doesn't support the video element.
+    </video>
 </section>
 
 <?php get_footer(); ?>
