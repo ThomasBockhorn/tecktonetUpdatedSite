fecc71a5e8f01533cb33ad91de99a7f329e730a2
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2019-03-05 14:19:50 -0600

    Changed the nav and main video

--- header.php
+++ header.php
@@ -8,16 +8,23 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
- <nav class="navbar">
- <!--This pertains to the navbar-->
-    <ul class="menu">
-        <li><a href="#" class="menu-item">Home</a></li>
-        <li><a href="#" class="menu-item">About Me</a></li>
-        <li><a href="#" class="menu-item">Projects</a></li>
-        <li><a href="#" class="menu-item">Blog</a></li>
-        <li><a href="#" class="menu-item">Links</a></li>
-    </ul>
-    <div class="hamburger">
-        <a href="#" id="navToggle"><span></span></a>
-    </div>
- </nav>
+<header id="main_section">
+    <nav class="navbar">
+        <!--This pertains to the navbar-->
+        <ul class="menu">
+            <li><a href="#" class="menu-item">Home</a></li>
+            <li><a href="#" class="menu-item">About Me</a></li>
+            <li><a href="#" class="menu-item">Projects</a></li>
+            <li><a href="#" class="menu-item">Blog</a></li>
+            <li><a href="#" class="menu-item">Links</a></li>
+        </ul>
+        <div class="hamburger">
+            <a href="#" id="navToggle"><span></span></a>
+        </div>
+    </nav>
+    <video width="100%" height="100%" playsinline autoplay muted loop>
+            <source src="<?php echo get_template_directory_uri() . '/dest/mov/Atoms.mp4' ?>" type="video/mp4">
+            Sorry, your browser doesn't support the video element.
+    </video>
+</header> 
+
